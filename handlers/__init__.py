806a523b8b9f8f009e1d56a2326e06c1732e20ab
MixaMixa30 <mihamihafil@gmail.com>
2025-04-11 11:32:07 +0300

    fix: Добавлена функция register_all_handlers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,10 +4,17 @@ from handlers.tasks import router as tasks_router
 from handlers.achievements import router as achievements_router
 from handlers.categories import router as categories_router
 
+from aiogram import Dispatcher
+
 # Список всех маршрутизаторов для подключения в основном файле
 routers = [
     common_router,
     tasks_router,
     achievements_router,
     categories_router
-] 
+]
+
+def register_all_handlers(dp: Dispatcher):
+    """Регистрирует все обработчики"""
+    for router in routers:
+        dp.include_router(router) 
